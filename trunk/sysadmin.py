f18fcc32836200cf9e31c26f0bc319c01c8ca433
Oneiroi <d.busby@saiweb.co.uk>
2009-06-19 08:48:47 +0000

    should of checked in last night! : bash verison of sysamdin script now moved to deprecated folder, python version initial checkin,
    git-svn-id: http://svn.saiweb.co.uk/branches/linux-the-sysadmin-script@371 26e0a314-53de-4590-a415-dc075c606953

---
+++ trunk/sysadmin.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+"""
+    @Author: David Busby (http://saiweb.co.uk)
+    @Program: sysadmin
+    @Description: Helper script for day to day sysadmin
+    @Copyright: Copyright (c) 2009 David Busby.
+    @License: http://creativecommons.org/licenses/by-sa/2.0/uk/ CC BY-SA
+"""
+import ConfigParser,os,sys,re,time,hashlib
+from zlib import crc32
+from optparse import OptionParser,OptionGroup, OptParseError
+
+
+
+class sysadmin:
+    
+    def __init__(self):
+        
+        self.var = ''
+        cwd = os.getcwd()
+        cfgPath = "%s%s" % (cwd,'/conf/sysadmin.conf')
+
+        cfg = ConfigParser.RawConfigParser()
+        cfg.read(cfgPath)
+
+        try:
+            self.v = cfg.getboolean('sysadmin', 'verbose')
+            self.homedir = cfg.get('core', 'homedir')
+            self.shell = cfg.get('core', 'shell')
+            self.adduser = cfg.get('core','adduser')
+            self.vhosts = cfg.get('core','vhosts')
+            self.mkdir = cfg.get('core','mkdir')
+            self.chown = cfg.get('core','chown')
+            
+        except ConfigParser.NoOptionError, e:
+            print e
+            sys.exit(0);
+            
+    def webuser(self,opts):
+        self.verbose('webuser()')
+        homedir = self.homedir % (opts['usr'])
+        cmd = self.adduser % (homedir, self.shell, opts['usr'])
+        self._exec(cmd)
+        cmd = self.mkdir % ('%s/%s' % (homedir,'public_html'))
+        self._exec(cmd)
+    
+    def _get_filesize(self,path):
+        return os.path.getsize(path)
+    
+    def _readfile(self,path):
+        self.verbose('_readfile(%s)' % (path))
+        #bytes before prompting occurs
+        limit = 30 * 1024 * 1024
+        
+        if self._get_filesize(path) > limit:
+            self.verbose('Large file detected')
+            size = round((self._get_filesize(path)/1024/1024),2) 
+            
+            print 'Large file detected, please ensure you have enough available memory to process this file before proceeding'
+            print 'It is not recomended that you process this file on a live server'
+            
+            response = raw_input('The file is %sMB, are you sure you wish to load this into memory? (y/n):' % (size))
+            
+            while response not in ('y','n'):
+                print 'Invalid response, please enter y or n'
+                response = raw_input('The file is %sMB, are you sure you wish to load this into memory? (y/n):' % (size))
+                
+            if response == 'n':
+                self.verbose('User decided not to proceed')
+                print 'Exiting on user request...'
+                sys.exit(0)
+            elif response == 'y':
+                self.verbose('User decided to proceed')
+                print 'Proceeding on user request ... be advised large files take a long time to process'
+                    
+        try:
+            f = file(path, 'r')
+            data = f.read()
+            f.close()
+        except IOError, e:
+            self.error(e)
+        return data
+    
+    def _checksum(self,data):
+        self.verbose('_checksum()')
+        m = hashlib.md5()
+        m.update(data)
+        return {'md5':m.hexdigest(),'crc32':crc32(data) & 0xffffffff}
+    
+    def _iconv(self,opts):
+        self.verbose('_iconv()')
+        tmp = opts
+        opts = iconv_opts()
+        try:
+            opts.path = tmp[0]
+            opts.cs_from = tmp[1]
+            opts.cs_to = tmp[2]
+        except IndexError, e:
+            self.verbose('Required data is missing')
+
+               
+        if (hasattr(opts, 'path') and opts.path != None) and (hasattr(opts, 'cs_from') and opts.cs_from != None) and (hasattr(opts, 'cs_to') and opts.cs_to != None):
+            sql = self._readfile(opts.path)
+            
+            if hasattr(opts, 'checksum') and opts.checksum == True:
+                src_checksum = self._checksum(sql)
+            try: 
+                tgt = unicode(sql,opts.cs_from).encode(opts.cs_to)
+            except UnicodeEncodeError, e:
+                self.error(e)
+            
+            try:
+                out_path = '%s.%s' % (opts.path,opts.cs_to)
+                f = file(out_path,'w+')
+                f.write(tgt)
+                f.close()
+            except IOError, e:
+                self.error(e)
+            
+            if hasattr(opts, 'checksum') and opts.checksum == True:
+                tgt_checksum = self._checksum(tgt)
+                
+                print 'src checksum: ', src_checksum
+                print 'tgt checksum: ', tgt_checksum
+                print 'output file: ', out_path
+                                         
+        else:
+            print 'Path, source charset and dest charset are required'
+            sys.exit(1)
+        
+    def appmem(self,filter):
+        self.verbose('appmem(%s)' % (filter))
+        cmd = 'ps aux | grep "%s" | grep -v "grep" | grep -v "%s"' % (filter,sys.argv[0])
+        data = self._exec(cmd)
+        data = data.split('\n')
+        raw = []
+        set = []
+        for line in data:
+            raw = line.split(' ')
+            dat = []
+            for tmp in raw:
+                if len(tmp) > 0:
+                    dat.append(tmp)
+            set.append(dat)
+        vsz = 0
+        rss = 0
+        
+        for line in set:
+            vsz += int(line[4])
+            rss += int(line[5])
+        count = len(set)
+        print '--- Memory Usage Report For %s ---' % (filter)
+        print 'PID Count: %s' % (count)
+        print 'Shared Memory Usage: %sMB' % (round((vsz/count)/1024,2))
+        print 'Total Resident Set Size: %sMB' % (round((rss/1024),2))
+        print 'MEM/PID: %sMB' % (round((rss/count)/1024,2)) 
+        
+        
+    def error(self,str):
+        self.verbose('error()')
+        print 'ERROR: ',str
+        sys.exit(1)
+        
+    def verbose(self,str):
+        if self.v:
+            print'%s: %s' % (time.time(),str)
+        
+    def _exec(self, cmd):
+        self.verbose('_exec(%s)' % (cmd))
+        prg = os.popen(cmd,"r")
+        str = ''
+        for line in prg.readlines():
+            str += line
+
+        return str.rstrip('\n')
+
+"""
+stubb class for iconv charset opts
+"""
+class iconv_opts:
+    path = None
+    cs_from = None
+    cs_to = None
+    checksum = True
+    
+def usage():
+  
+    
+    help = """
+    
+    ### Sysadmin Script by D.Busby                                          ###
+    ### http://saiweb.co.uk                                                 ###
+    ### license: http://creativecommons.org/licenses/by-sa/2.0/uk/ CC BY-SA ###
+    
+    %s -c command -d csv,seperated,data
+    
+    Available commands:
+    
+        iconv - This command is used to converted between charecter encoding
+        Example: -c iconv -d /path/to/file.ext,latin-1,utf-8
+        
+        appmem - This commaned is used to estimate the memory usage of a currently running process
+        Example: -c appmem -d filter
+        Note: filter can be the process name i.e. httpd or anything else you wish to filter by i.e. PID
+        
+        webuser - This command is used to create a new webuser
+        Example: -c webuser -d username
+        Notes: The majority of the configuration for this command take place under the advanced section of the config file
+    """ % (sys.argv[0])
+    
+    return help
+
+def main():         
+    sa = sysadmin()
+    parser = OptionParser(usage=usage(), version="%prog 1.0")
+    parser.add_option('-c','--command', dest='command', help='Command to run')
+    parser.add_option('-d','--data', dest='data', help='CSV Style data')
+    
+    (options,args) = parser.parse_args()
+    
+    if options.command == None:
+        parser.error('Command is a required input')
+    elif options.data == None:
+        parser.error('Data is a required input')
+    else:
+        opts = options.data.split(',')
+        
+        values = {
+                  'iconv': lambda: sa._iconv(opts)
+                  }
+        
+        values.get(options.command,usage())
+    
+       
+    
+                
+if __name__ == "__main__":
+    main()
