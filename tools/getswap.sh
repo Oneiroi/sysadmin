c13ff921c77f53932ea3246831ff1c2acfd27539
David Busby <david.busby@psycle.com>
2012-08-01 11:31:19 +0100

    Simple header updates with versioning etc

--- tools/getswap.sh
+++ tools/getswap.sh
@@ -2,7 +2,9 @@
 
 #
 # Very quick script to get current swap usage
-# David Busby oneiroi@fedoraproject.org
-#
+# Author: David Busby oneiroi@fedoraproject.org http://blog.oneiroi.co.uk
+# Changes:
+#    v1.0 01 Aug 2012 Initial Release
+# 
 
 ls /proc/ | egrep '[0-9]' | while read pid; do echo "`ps -p $pid -o comm --no-headers` `grep Swap /proc/$pid/smaps | awk '{ x += $2 } END { print x }'`"; done | awk '{ y[$1] += $2 } END { for ( k in y ) { print k, y[k] }}'
