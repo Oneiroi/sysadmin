d94c3a9ca7ee8771d70966ecad16d6c4eff444b6
David Busby <david.busby@psycle.com>
2012-09-25 16:24:51 +0100

    Added wrapper to hopefully have zenoss produce warnings correctly

--- nagios/libexec/check_mysql_replication.sh
+++ nagios/libexec/check_mysql_replication.sh
@@ -12,6 +12,10 @@ while getopts "a:b:u:p:s" opt; do
         [?]) echo "Invalid option $opt" && exit 2;;
     esac
 done
+#added -l 1000 -w 500 25/06/2012
+/usr/bin/python /usr/local/zenoss/common/libexec/check_mysql_replication.py -a $A -b $B -u $U -p $P -l 1000 -w 500
+check_code=$?
 
-/usr/bin/python /usr/local/zenoss/common/libexec/check_mysql_replication.py -a $A -b $B -u $U -p $P
-exit $?
+[[ $check_code -eq 0 ]] && exit $check_code;
+[[ $check_code -eq 1 ]] && echo -n '| Severity=Warning' && exit $check_code;
+[[ $check_code -eq 2 ]] && echo -n '| Severity=Critical' && exit $check_code;
