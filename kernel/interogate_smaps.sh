2483b444b8313e24afb25cd00a4bc1c4702ddc38
David Busby <oneiroi@fedoraproject.org>
2013-01-28 09:22:30 +0000

    full smaps interogation

--- kernel/interogate_smaps.sh
+++ kernel/interogate_smaps.sh
@@ -5,6 +5,22 @@
 # Author: David Busby oneiroi@fedoraproject.org http://blog.oneiroi.co.uk
 # Changes:
 #    v1.0 01 Aug 2012 Initial Release
-# 
+#    v1.1 28 Jan 2013 renamed getswap.sh to interogate_smaps.sh provides full summary of snmaps kbytes
 
-ls /proc/ | egrep '[0-9]' | while read pid; do echo "`ps -p $pid -o comm --no-headers` `grep Swap /proc/$pid/smaps | awk '{ x += $2 } END { print x }'`"; done | awk '{ y[$1] += $2 } END { for ( k in y ) { print k, y[k] }}'
+
+smaps_summary(){
+  if [ -f "/proc/$1/smaps" ]; then
+    /usr/bin/cat /proc/$1/smaps |\
+    egrep 'Size|Rss|Pss|Shared_|Private|Reference|Anon|Swap|Kernel|MMU|Locked' | /usr/bin/sort -k1 |\
+    awk '{x[$1] += $2} END { for ( k in x ) { printf "%s %s\t",k,x[k]}}'
+    echo ""
+  else
+    echo "NO SMAPS FILE";
+  fi
+}
+
+ls /proc/ | egrep '[0-9]' | \
+while read pid; do \
+  echo -n "`ps -p $pid -o comm --no-headers`  ";
+  smaps_summary $pid
+done
