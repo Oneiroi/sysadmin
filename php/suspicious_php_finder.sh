d9eb315681fd8730c06683b3bec31dceb27af387
David Busby <oneiroi@fedoraproject.org>
2015-07-02 11:56:18 +0100

    Removed -v flag unsure why I had set this inverse matching, really odd

--- php/suspicious_php_finder.sh
+++ php/suspicious_php_finder.sh
@@ -14,14 +14,14 @@ echo '[INFO] Producing filelist *.php > $TMP/files'
 find $1 -noleaf -type f -name '*.php' -print0 > $TMP/files
 echo '[INFO] Completed $TMP/files'
 echo '[HIGH] Search for high likelyhood compromises'
-cat $TMP/files | xargs --null -P0 egrep -iIH -v 'eval[[:space:]]+?\([[:space:]]+?(base64|gzinflate|edoced)'
+cat $TMP/files | xargs --null -P0 egrep -iIH 'eval[[:space:]]+?\([[:space:]]+?(base64|gzinflate|edoced)'
 echo '[MEDIUM] PASS 1 - Search for medium likelyhood compromises, may produce a lot of false positives if you frequently use os exec functions legitimately'
-cat $TMP/files | xargs --null -P0 egrep -iIH -v 'ex|exec|passthru|system|pcntl_|proc_|posix_|ssh2_|ssh_'
+cat $TMP/files | xargs --null -P0 egrep -iIH 'ex|exec|passthru|system|pcntl_|proc_|posix_|ssh2_|ssh_'
 echo '[MEDIUM] PASS 2 - Search for medium likelyhood compromises, may produce a lot of false positives if you use hex strings legitimately'
 cat $TMP/files | xargs --null -P0 egrep -iIH '\x'
 echo '[MEDIUM] PASS 3 - Search for medium likelyhood compromises, may produce a lot of false positives if you ord or pack legitimately'
-cat $TMP/files | xargs --null -P0 egrep -iIH -v 'ord|pack[[:space:]]+?\('
+cat $TMP/files | xargs --null -P0 egrep -iIH 'ord|pack[[:space:]]+?\('
 echo '[LOW] Search for low likelyhood compromises, may produce a lot of false positives if you use ini_* legitimately'
-cat $TMP/files | xargs --null -P0 egrep -iIH -v 'ini_.*[[:space:]]+?\('
+cat $TMP/files | xargs --null -P0 egrep -iIH 'ini_.*[[:space:]]+?\('
 
 
