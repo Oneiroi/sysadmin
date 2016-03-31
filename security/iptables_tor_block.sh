ae8ef206c6399c16caec95041e869919ada13f27
David Busby <oneiroi@fedoraproject.org>
2016-03-31 11:08:43 +0100

    iptables_tor_block rough script to create a new chain from list of known tor exit nodes, and write the appropriate rules for them

---
+++ security/iptables_tor_block.sh
@@ -0,0 +1,4 @@
+#!/bin.bash
+iptables -N TOR
+iptables -I INPUT -j TOR
+curl https://check.torproject.org/exit-addresses | grep ExitAddress | awk '{print $2}' |  sort  | awk '{print "iptables -I TOR -s "$1" -j DROP"}'
