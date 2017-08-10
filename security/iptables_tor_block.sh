1c3d459e79a8f8a81610ec982f6c2290541118a6
David Busby <oneiroi@fedoraproject.org>
2017-08-10 11:41:29 +0100

    Mkae NOOP & write this fact out to stdout

--- security/iptables_tor_block.sh
+++ security/iptables_tor_block.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-iptables -N TOR
-iptables -I INPUT -j TOR
+echo "#[NOOP] No actions will be taken on your system, you will need to copy & paste this output"
+echo "iptables -N TOR"
+echo "iptables -I INPUT -j TOR"
 curl -s https://check.torproject.org/exit-addresses | grep ExitAddress | awk '{print $2}' |  sort  | awk '{print "iptables -I TOR -s "$1" -j DROP"}'
