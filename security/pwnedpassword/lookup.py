f4952a71bc42ef6837baac37520563dff8cc266d
David Busby <oneiroi@fedoraproject.org>
2022-07-18 14:51:05 +0100

    Update to be python3 compatable

--- security/pwnedpassword/lookup.py
+++ security/pwnedpassword/lookup.py
@@ -4,11 +4,10 @@ from getpass import getpass
 from requests import get
 
 def main():
-    print "This script will check your password against pwnedpasswords.com"
-    print "This uses the k-Anonymity model, meaning we only lookup the first 5 chars of the sha1 digest of your password"
-    print "Your password is never sent to pwnedpasswords this way"
-    print
-    passHash = sha1(getpass("Please provide the password to check: ")).hexdigest().upper()
+    print("This script will check your password against pwnedpasswords.com")
+    print("This uses the k-Anonymity model, meaning we only lookup the first 5 chars of the sha1 digest of your password")
+    print("Your password is never sent to pwnedpasswords this way")
+    passHash = sha1(getpass("Please provide the password to check: ").encode("utf-8")).hexdigest().upper()
     req  = get("https://api.pwnedpasswords.com/range/%s" % passHash[:5])
     if req:
         #pwnedpasswords API returns the hash suffix:count, with passHash[:5] being the hash prefix
@@ -16,13 +15,12 @@ def main():
         for line in req.text.split('\n'):
             if passHash[:5] + line.split(':')[0] == passHash:
                 #We have a match, print this out
-                print 
-                print "[WARN] Your password was noted on pwnedpasswords %s times" % line.split(':')[1].strip()
+                print ("[WARN] Your password was noted on pwnedpasswords %s times" % line.split(':')[1].strip())
                 return
     #Clean up (Just to err on the side of paranoia)
     del passHash
     #_IF_ we are here, we can assume that there has been no matches
-    print "[INFO] Good news, I could not find a match on api.pwnedpasswords.com, this does not count as endorsement for this password however"
+    print("[INFO] Good news, I could not find a match on api.pwnedpasswords.com, this does not count as endorsement for this password however")
 
 if __name__ == '__main__':
     main()
